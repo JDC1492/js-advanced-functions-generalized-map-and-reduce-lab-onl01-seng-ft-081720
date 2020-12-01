99c12ddefac369d60b4173cbd92bb8bfcd683c77
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-01 13:45:15 -0500

    commit tree ff35185dff31b8d2b7e6e3f1621e55da0f5123ff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+function map(arr, func){
+        return arr.map(x => func(x))
+    }
+
+function reduce(sourceArray, func, startingPoint){
+        let r = (!!startingPoint) ? startingPoint : sourceArray[0]
+        let i = (!!startingPoint) ? 0 : 1
+        for (; i < sourceArray.length; i++) {
+          r = func(sourceArray[i], r)
+        }
+        return r;
+      }
